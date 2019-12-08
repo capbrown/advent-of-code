11a02ec21f840b3a08e7026d96725d0af3507874
Christopher Brown <cbrow247@uwo.ca>
2019-12-08 15:27:13 -0500

    Completed day 8 part 2

--- day8.py
+++ day8.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open('inputs/day8.txt', 'r') as f:
     image = next(f)
@@ -22,4 +23,20 @@ for l in range(n_layers):
 
 unique, counts = np.unique(arr[min_layer, :, :], return_counts=True)
 dic = dict(zip(unique, counts))
-print(dic[1] * dic[2])
+print("Part 1:", dic[1] * dic[2])
+
+decoded_image = np.empty((height, width))
+decoded_image[:] = np.nan
+
+for l in range(n_layers):
+
+    layer = arr[l, :, :]
+    layer_i = np.nonzero(np.logical_or(layer == 0, layer == 1))
+
+    for i in range(len(layer_i[0])):
+        ind = tuple([layer_i[0][i], layer_i[1][i]])
+        if np.isnan(decoded_image[ind]):
+            decoded_image[ind] = layer[ind]
+
+plt.imshow(decoded_image)
+plt.show()
