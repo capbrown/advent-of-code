c0914cd7e990a8682df6033e2a6e9c1d0f547f8b
Christopher Brown <cbrow247@uwo.ca>
2019-12-14 15:49:58 -0500

    Completed day 10 part 2

--- day10.py
+++ day10.py
@@ -16,6 +16,7 @@ a = np.transpose(np.nonzero(arr))
 all_asteroids = list(map(tuple, a))
 
 max_asteroids = 0
+max_i = None
 for asteroid in all_asteroids:
 
     # print("asteroid =", asteroid)
@@ -60,5 +61,62 @@ for asteroid in all_asteroids:
 
     if len(visible_asteroids) > max_asteroids:
         max_asteroids = len(visible_asteroids)
+        max_i = asteroid
 
-print(max_asteroids)
+print('Part 1:', max_asteroids)
+
+# part 2
+alla = all_asteroids[:]
+asteroid = max_i
+removed = []
+while True:
+    other_asteroids = all_asteroids[:]
+    other_asteroids.remove(asteroid)
+    visible_asteroids = []
+
+    for current_asteroid in other_asteroids:
+
+        angle_to_asteroid = tuple(np.subtract(current_asteroid, asteroid))
+        # simplify angle 'fraction'
+        if 0 in angle_to_asteroid:
+            if angle_to_asteroid[0] == 0:
+                new_y = angle_to_asteroid[0]
+                new_x = angle_to_asteroid[1] // np.abs(angle_to_asteroid[1])
+                ang_sort = 90 if new_x > 0 else 270
+            else:
+                new_y = angle_to_asteroid[0] // np.abs(angle_to_asteroid[0])
+                new_x = angle_to_asteroid[1]
+                ang_sort = 0 if new_y < 0 else 180
+        else:
+            f = Fraction(angle_to_asteroid[0], angle_to_asteroid[1])
+            new_y = np.abs(f.numerator) * angle_to_asteroid[0] / np.abs(angle_to_asteroid[0])
+            new_x = np.abs(f.denominator) * angle_to_asteroid[1] / np.abs(angle_to_asteroid[1])
+            if new_y > 0 and new_x > 0:
+                ang_sort = 90 + np.abs(np.degrees((np.arctan((-1 * new_y / new_x)))))
+            elif new_y > 0 and new_x < 0:
+                ang_sort = 270 - np.degrees((np.arctan((-1 * new_y / new_x))))
+            elif new_y < 0 and new_x > 0:
+                ang_sort = 90 - np.degrees((np.arctan((-1 * new_y / new_x))))
+            elif new_y < 0 and new_x < 0:
+                ang_sort = 270 + np.abs(np.degrees((np.arctan((-1 * new_y / new_x)))))
+
+        # See if any multiples of this base fraction exist that would block visibility
+        blocked = False
+        current_check = tuple(np.add(asteroid, (new_y, new_x)))
+
+        while current_check != current_asteroid:
+            if current_check in other_asteroids:
+                blocked = True
+            current_check = tuple(np.add(current_check, (new_y, new_x)))
+
+        if not blocked:
+            visible_asteroids.append((current_asteroid, ang_sort))
+
+    # sort visible asteroids based on their angle relative to the vertical
+    sort_a = sorted(visible_asteroids, key=lambda asa: asa[1])
+    removed.extend([a[0] for a in sort_a])
+
+    if len(removed) >= 200:
+        break
+
+print('Part 2:', removed[199][1] * 100 + removed[199][0])
